3f5b468122c9aad1d4480f8cc47bfde2a56450f7
Beatriz Castro <beatriz_arruda_castro@hotmail.com>
2021-04-27 22:08:10 -0300

    configurar layout de telas

---
+++ MyGames/AddEditViewController.swift
@@ -0,0 +1,43 @@
+//
+//  AddEditViewController.swift
+//  MyGames
+//
+//  Created by Beatriz Castro on 27/04/21.
+//
+
+import UIKit
+
+class AddEditViewController: UIViewController {
+    
+    @IBOutlet weak var tfTitle: UITextField!
+    @IBOutlet weak var tfConsole: UITextField!
+    @IBOutlet weak var dpReleaseDate: UIDatePicker!
+    @IBOutlet weak var btAddEdit: UIButton!
+    @IBOutlet weak var btCover: UIButton!
+    @IBOutlet weak var ivCover: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    
+    @IBAction func addEditCover(_ sender: Any) {
+    }
+    
+    @IBAction func addEditGame(_ sender: Any) {
+    }
+    
+}
