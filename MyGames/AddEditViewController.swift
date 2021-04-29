bb8e3600f89e19bba76c0afc942da54bf9a9cc30
Beatriz Castro <beatriz_arruda_castro@hotmail.com>
2021-04-29 20:49:53 -0300

    adicionar consoles e relacionar aos games

--- MyGames/AddEditViewController.swift
+++ MyGames/AddEditViewController.swift
@@ -18,12 +18,66 @@ class AddEditViewController: UIViewController {
     @IBOutlet weak var btCover: UIButton!
     @IBOutlet weak var ivCover: UIImageView!
     
+    lazy var pickerView: UIPickerView = {
+           let pickerView = UIPickerView()
+           pickerView.delegate = self
+           pickerView.dataSource = self
+           pickerView.backgroundColor = .white
+           return pickerView
+       }()
+    
+   var consolesManager = ConsolesManager.shared
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        consolesManager.loadConsoles(with: context)
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        prepareDataLayout()
+    }
+    
+    func prepareDataLayout() {
+        if game != nil {
+            title = "Editar jogo"
+            btAddEdit.setTitle("ALTERAR", for: .normal)
+            tfTitle.text = game.title
+            
+            // tip. alem do console pegamos o indice atual para setar o picker view
+            if let console = game.console, let index = consolesManager.consoles.index(of: console) {
+                tfConsole.text = console.name
+                pickerView.selectRow(index, inComponent: 0, animated: false)
+            }
+            ivCover.image = game.cover as? UIImage
+            if let releaseDate = game.releaseDate {
+                dpReleaseDate.date = releaseDate
+            }
+            if game.cover != nil {
+                btCover.setTitle(nil, for: .normal)
+            }
+        }
+        
+        let toolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: 44))
+        toolbar.tintColor = UIColor(named: "main")
+        let btCancel = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(cancel))
+        let btDone = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(done))
+        let btFlexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        toolbar.items = [btCancel, btFlexibleSpace, btDone]
+        
+        // tip. faz o text field exibir os dados predefinidos pela picker view
+        tfConsole.inputView = pickerView
+        tfConsole.inputAccessoryView = toolbar
+    }
+    
+    @objc func cancel() {
+        tfConsole.resignFirstResponder()
+    }
+    
+    @objc func done() {
+        tfConsole.text = consolesManager.consoles[pickerView.selectedRow(inComponent: 0)].name
+        cancel()
+    }
 
     /*
     // MARK: - Navigation
@@ -46,6 +100,13 @@ class AddEditViewController: UIViewController {
         }
         game.title = tfTitle.text
         game.releaseDate = dpReleaseDate.date
+        
+        if !tfConsole.text!.isEmpty {
+              let console = consolesManager.consoles[pickerView.selectedRow(inComponent: 0)]
+              game.console = console
+        }
+        game.cover = ivCover.image
+        
         do {
           try context.save()
         } catch {
@@ -55,4 +116,22 @@ class AddEditViewController: UIViewController {
         navigationController?.popViewController(animated: true)
     }
     
+}
+
+
+extension AddEditViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+ 
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+ 
+ 
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return consolesManager.consoles.count
+    }
+ 
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        let console = consolesManager.consoles[row]
+        return console.name
+    }
 }
